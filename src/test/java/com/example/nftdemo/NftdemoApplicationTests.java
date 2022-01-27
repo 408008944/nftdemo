695fee4e9cd1a4d016df94266a421846e6ce0b05
unknown <you@example.com>
2022-01-27 10:35:34 +0800

    first commit

---
+++ src/test/java/com/example/nftdemo/NftdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nftdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NftdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
